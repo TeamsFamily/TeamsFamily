5a94eb155e7e7cad877e45d3e8afd962899858d7
Ahmadfigo <ahmadfigo@programmer.net>
2019-11-23 23:08:44 +0700

    NGELU COK

---
+++ application/views/index.php
@@ -0,0 +1,300 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Dashboard One | Notika - Notika Admin Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>assets/img/favicon.ico">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/font-awesome.min.css">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/owl.carousel.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/owl.theme.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/owl.transitions.css">
+    <!-- meanmenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/meanmenu/meanmenu.min.css">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/animate.css">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/normalize.css">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/scrollbar/jquery.mCustomScrollbar.min.css">
+    <!-- jvectormap CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/jvectormap/jquery-jvectormap-2.0.3.css">
+    <!-- notika icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/notika-custom-icon.css">
+    <!-- wave CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/wave/waves.min.css">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/main.css">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/style.css">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/responsive.css">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- Start Header Top Area -->
+    <div class="header-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="logo-area">
+                        <a href="#"><img src="<?=base_url()?>assets/img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <div class="header-top-menu">
+                        <ul class="nav navbar-nav notika-top-nav">
+                            <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><span><i class="notika-icon notika-chat"></i></span></a>
+                                <div role="menu" class="dropdown-menu message-dd chat-dd animated zoomIn">
+                                    <div class="hd-mg-tt">
+                                        <h2>Chat</h2>
+                                    </div>
+                                    <div class="search-people">
+                                        <i class="notika-icon notika-left-arrow"></i>
+                                        <input type="text" placeholder="Search People" />
+                                    </div>
+                                    <div class="hd-message-info">
+                                        <a href="#">
+                                            <div class="hd-message-sn">
+                                                <div class="hd-message-img chat-img">
+                                                    <img src="<?=base_url()?>assets/img/post/1.jpg" alt="" />
+                                                    <div class="chat-avaible"><i class="notika-icon notika-dot"></i></div>
+                                                </div>
+                                                <div class="hd-mg-ctn">
+                                                    <h3>David Belle</h3>
+                                                    <p>Available</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="hd-message-sn">
+                                                <div class="hd-message-img chat-img">
+                                                    <img src="<?=base_url()?>assets/img/post/2.jpg" alt="" />
+                                                </div>
+                                                <div class="hd-mg-ctn">
+                                                    <h3>Jonathan Morris</h3>
+                                                    <p>Last seen 3 hours ago</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="hd-message-sn">
+                                                <div class="hd-message-img chat-img">
+                                                    <img src="<?=base_url()?>assets/img/post/4.jpg" alt="" />
+                                                </div>
+                                                <div class="hd-mg-ctn">
+                                                    <h3>Fredric Mitchell</h3>
+                                                    <p>Last seen 2 minutes ago</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="hd-message-sn">
+                                                <div class="hd-message-img chat-img">
+                                                    <img src="<?=base_url()?>assets/img/post/1.jpg" alt="" />
+                                                    <div class="chat-avaible"><i class="notika-icon notika-dot"></i></div>
+                                                </div>
+                                                <div class="hd-mg-ctn">
+                                                    <h3>David Belle</h3>
+                                                    <p>Available</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="hd-message-sn">
+                                                <div class="hd-message-img chat-img">
+                                                    <img src="<?=base_url()?>assets/img/post/2.jpg" alt="" />
+                                                    <div class="chat-avaible"><i class="notika-icon notika-dot"></i></div>
+                                                </div>
+                                                <div class="hd-mg-ctn">
+                                                    <h3>Glenn Jecobs</h3>
+                                                    <p>Available</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="hd-mg-va">
+                                        <a href="#">View All</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Top Area -->
+    <!-- Mobile Menu start -->
+    <div class="mobile-menu-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="mobile-menu">
+                        <nav id="dropdown">
+                            <ul class="mobile-menu-nav">
+                                <li><a href="#"><i class="notika-icon notika-house"></i>Beranda</a>
+                                </li>
+                                <li><a href="<?=base_url()?>Email"><i class="notika-icon notika-mail"></i> Email</a>
+                                </li>
+                                <li><a data-toggle="tab" href="#Interface"><i class="notika-icon notika-edit"></i> Interface</a>
+                                </li>
+                                <li><a data-toggle="tab" href="#Charts"><i class="notika-icon notika-bar-chart"></i> Charts</a>
+                                </li>
+                                <li><a data-toggle="tab" href="#Tables"><i class="notika-icon notika-windows"></i> Tables</a>
+                                </li>
+                                <li><a data-toggle="tab" href="#Forms"><i class="notika-icon notika-form"></i> Forms</a>
+                                </li>
+                                <li><a data-toggle="tab" href="#Appviews"><i class="notika-icon notika-app"></i> App views</a>
+                                </li>
+                                <li><a data-toggle="tab" href="#Page"><i class="notika-icon notika-support"></i> Pages</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Mobile Menu end -->
+    <!-- Main Menu area start-->
+    <div class="main-menu-area mg-tb-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
+                        <li><a href="#"><i class="notika-icon notika-house"></i>Beranda</a>
+                        </li>
+                        <li><a href="<?=base_url()?>Email"><i class="notika-icon notika-book"></i> Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Main Menu area End-->
+    <!-- Start Status area -->
+    <div class="realtime-statistic-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-6 col-sm-6 col-xs-12">
+                    <div class="realtime-wrap notika-shadow mg-t-30">
+                        <div class="realtime-ctn">
+                            <div class="realtime-title">
+                                <h2>Selamat Datang</h2>
+                            </div>
+                            <div class="realtime-title">
+                                <h2>Di Website Pencatat Keuangan Keluarga</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Realtime sts area-->
+    <!-- Start Footer area-->
+    <!-- End Footer area-->
+    <!-- jquery
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/wow.min.js"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/jquery-price-slider.js"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/owl.carousel.min.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/jquery.scrollUp.min.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/meanmenu/jquery.meanmenu.js"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/counterup/jquery.counterup.min.js"></script>
+    <script src="<?=base_url()?>assets/js/counterup/waypoints.min.js"></script>
+    <script src="<?=base_url()?>assets/js/counterup/counterup-active.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- jvectormap JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="<?=base_url()?>assets/js/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="<?=base_url()?>assets/js/jvectormap/jvectormap-active.js"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/sparkline/jquery.sparkline.min.js"></script>
+    <script src="<?=base_url()?>assets/js/sparkline/sparkline-active.js"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/flot/jquery.flot.js"></script>
+    <script src="<?=base_url()?>assets/js/flot/jquery.flot.resize.js"></script>
+    <script src="<?=base_url()?>assets/js/flot/curvedLines.js"></script>
+    <script src="<?=base_url()?>assets/js/flot/flot-active.js"></script>
+    <!-- knob JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/knob/jquery.knob.js"></script>
+    <script src="<?=base_url()?>assets/js/knob/jquery.appear.js"></script>
+    <script src="<?=base_url()?>assets/js/knob/knob-active.js"></script>
+    <!--  wave JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/wave/waves.min.js"></script>
+    <script src="<?=base_url()?>assets/js/wave/wave-active.js"></script>
+    <!--  todo JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/todo/jquery.todo.js"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/plugins.js"></script>
+	<!--  Chat JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/chat/moment.min.js"></script>
+    <script src="<?=base_url()?>assets/js/chat/jquery.chat.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="<?=base_url()?>assets/js/main.js"></script>
+	<!-- tawk chat JS
+		============================================ -->
+    <!-- <script src="<?=base_url()?>assets/js/tawk-chat.js"></script> -->
+</body>
+
+</html>
